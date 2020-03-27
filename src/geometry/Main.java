fb4d706c756bc7dd6f9ab646e1d18ac3b613962f
phi <daovanphi25051994@gmail.com>
2020-03-27 15:35:13 +0700

    fixed main method

--- src/geometry/Main.java
+++ src/geometry/Main.java
@@ -3,6 +3,7 @@ package geometry;
 
 public class Main {
     public static void main(String[] args) {
+        //test polymorphic of array geometries
         Geometry[] geometries = new Geometry[3];
         geometries[0] = new Circle();
         geometries[1] = new Rectangle();
@@ -13,6 +14,15 @@ public class Main {
         }
         for (Geometry geometry : geometries) {
             System.out.println(geometry);
+        }
+        //test class square implement interface colorable
+        System.out.println();
+        for (Geometry geometry : geometries) {
+            if (geometry instanceof Square) {
+                ((Square) geometry).howToColor();
+            } else {
+                System.out.println(geometry);
+            }
         }
     }
 }
